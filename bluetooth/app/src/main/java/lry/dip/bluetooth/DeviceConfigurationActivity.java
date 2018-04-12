cd45fae02e00c0f63715426afe41275626be9708
Raphael-T <raphael.tison@hotmail.fr>
2018-04-12 13:34:09 +0200

    Initialisation

---
+++ bluetooth/app/src/main/java/lry/dip/bluetooth/DeviceConfigurationActivity.java
@@ -0,0 +1,15 @@
+package lry.dip.bluetooth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rtison on 12/04/2018.
+ */
+
+public class DeviceConfigurationActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_device_configuration);
+    }
+}
