4fb58b266924dc589be76c48ecd1b990685e0961
JohanTerrenoire <terrenoirejohan@yahoo.fr>
2018-04-12 15:07:13 +0200

    Démarrage activité lancement

---
+++ bluetooth/app/src/main/java/lry/dip/launcher/launcher.java
@@ -0,0 +1,14 @@
+package lry.dip.launcher;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import lry.dip.bluetooth.R;
+
+public class launcher extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);//Définir la vue
+
+    }
+}
